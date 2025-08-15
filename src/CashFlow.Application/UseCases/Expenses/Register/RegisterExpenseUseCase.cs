869a602a6e3c731e42c366cc41f59484eacff95e
Allan Fantoni <allan.fantoni@outlook.com>
2025-08-14 23:03:32 -0300

    Validate expenses using if

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -7,8 +8,29 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        // Validate the request
+        Validate(request);
 
         return new ResponseRegisterExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        
+        if (titleIsEmpty)
+            throw new ArgumentException("Title cannot be empty.", nameof(request.Title));
+
+        if (request.Amount <= 0)
+            throw new ArgumentException("Amount must be greater than zero.", nameof(request.Amount));
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+        if (result > 0)
+            throw new ArgumentException("Date cannot be in the future.", nameof(request.Date));
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+        if (!paymentTypeIsValid)
+            throw new ArgumentException("Invalid payment type.", nameof(request.PaymentType));
     }
 }
