5af3877f9d1761ca4c0bb403e29b06afcecbdc1d
Allan Fantoni <allan.fantoni@outlook.com>
2025-12-18 08:39:53 -0300

    Configure DbContext

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = @"Server=localhost\\SQLEXPRESS;
+                                 Database=cashflowdb;
+                                 User Id=sa;
+                                 Password=manager;
+                                 TrustServerCertificate=True;";
+        
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
